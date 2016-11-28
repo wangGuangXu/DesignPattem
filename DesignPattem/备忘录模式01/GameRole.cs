109f60946b64091048d6ec79119593ea034c7012
wgx0428 <656052658@qq.com>
2016-11-28 18:04:18 +0800

    备忘录模式

---
+++ DesignPattem/备忘录模式01/GameRole.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 备忘录模式01
+{
+    /// <summary>
+    /// 游戏角色类
+    /// </summary>
+    public class GameRole
+    {
+        /// <summary>
+        /// 生命力
+        /// </summary>
+        private int vit;
+        public int Vitality
+        {
+            get { return vit; }
+            set { vit = value; }
+        }
+
+        /// <summary>
+        /// 攻击力
+        /// </summary>
+        private int atk;
+        public int Attack
+        {
+            get { return atk; }
+            set { atk = value; }
+        }
+
+        /// <summary>
+        /// 防御力
+        /// </summary>
+        private int def;
+        public int Defense
+        {
+            get { return def; }
+            set { def = value; }
+        }
+
+        /// <summary>
+        /// 状态显示
+        /// </summary>
+        public void StateDisplay()
+        {
+            Console.WriteLine("角色当前状态： ");
+            Console.WriteLine("体力{0}",this.vit);
+            Console.WriteLine("攻击力{0}",this.atk);
+            Console.WriteLine("防御力{0}",this.def);
+
+            Console.WriteLine("");
+        }
+
+        /// <summary>
+        /// 获得初始状态
+        /// </summary>
+        public void GetInitState()
+        {
+            this.vit = 100;
+            this.atk = 100;
+            this.def = 100;
+        }
+
+        /// <summary>
+        /// 战斗
+        /// </summary>
+        public void Fight()
+        {
+            this.vit = 0;
+            this.atk = 0;
+            this.def = 0;
+        }
+
+    }
+}
---
+++ DesignPattem/备忘录模式01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 备忘录模式01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //大战 Boss 前
+            GameRole lixiaoyao = new GameRole();
+        }
+    }
+}
