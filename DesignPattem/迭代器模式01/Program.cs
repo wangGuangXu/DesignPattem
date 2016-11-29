a8c9741ea72718f808ba65273f707065697430dc
wgx0428 <656052658@qq.com>
2016-11-29 14:31:51 +0800

    添加迭代器模式UML类图

---
+++ DesignPattem/迭代器模式01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 迭代器模式01
+{
+    /// <summary>
+    /// 迭代器模式 提供一种方式顺序访问一个聚合对象中各个元素，而又不暴露该对象的内部表示
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
