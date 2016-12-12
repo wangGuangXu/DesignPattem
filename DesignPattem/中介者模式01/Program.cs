0e4cc8fd2ed0bb2c4ea0ebef2e6591fb3e2b360f
wgx0428 <656052658@qq.com>
2016-12-12 17:39:05 +0800

    新增中介者模式

---
+++ DesignPattem/中介者模式01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 中介者模式01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
