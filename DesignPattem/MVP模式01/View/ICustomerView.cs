3cf8fd5750259153c00c1594531015d74ed66834
wgx0428 <656052658@qq.com>
2016-12-21 14:00:06 +0800

    新增MVP关系UML类图

--- DesignPattem/MVP模式01/View/ICustomerView.cs
+++ DesignPattem/MVP模式01/View/ICustomerView.cs
@@ -10,14 +10,30 @@ namespace MVP模式01.View
     /// </summary>
     public interface ICustomerView
     {
+        /// <summary>
+        /// 客户列表
+        /// </summary>
         IList<string> Customers { get; set; }
-
+        /// <summary>
+        /// 当前选中客户
+        /// </summary>
         int SelectedCustomer { get; set; }
-
+        /// <summary>
+        /// 客户姓名
+        /// </summary>
         string CustomerName { get; set; }
-        string Address { get; set; }
+        /// <summary>
+        /// 客户手机号
+        /// </summary>
         string Phone { get; set; }
+        /// <summary>
+        /// 客户地址
+        /// </summary>
+        string Address { get; set; }
 
+        /// <summary>
+        /// MVP模式中控制的核心
+        /// </summary>
         Presenter.CustomerPresenter Presenter { set; }
     }
 }
